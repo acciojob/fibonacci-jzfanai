4714d075ccbfbafc7cda966d8f3e31f9fe60cd7d
jzfanai <161463249+jzfanai@users.noreply.github.com>
2024-09-11 03:05:10 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function fibonacci(num) {
-// your code here
+    if (num < 0) {
+        throw new Error("Input must be a non-negative integer.");
+    }
+    if (num === 0) {
+        return 0;
+    }
+    if (num === 1) {
+        return 1;
+    }
+    
+    let a = 0; // F(0)
+    let b = 1; // F(1)
+    let fib = 1; // Start with the second term
+    
+    for (let i = 2; i <= num; i++) {
+        fib = a + b;
+        a = b;
+        b = fib;
+    }
+    
+    return fib;
 }
 
 module.exports = fibonacci;
